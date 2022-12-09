c14312b8cf8ef5b0022c9d4b3c2dd8bb0aada26a
Daniela Leguari <danielaleguari@outlook.com>
2022-12-09 11:09:34 -0300

    atualização de epsódios e ajustes css

--- src/components/BannerSobreASerie/index.tsx
+++ src/components/BannerSobreASerie/index.tsx
@@ -11,7 +11,7 @@ const BannerSobreSerie = () => {
                     <img className={styles.bannerImg} src={imagem2} alt="Os Winchesters"></img>
                     <h1 className={styles.titulo}>Os Winchesters</h1>
                     <p className={styles.texto}>A Série "The Winchesters" é um spin-off de Supernatural. Sua estreia foi em Outubro deste ano.
-                        A série mostra como os pais de Dean e Sam se conheceram e tornaram-se tão envolvidos em questões sobrenaturais. </p>
+                         </p>
                 </div>
             </main>
 
