6c5f39021549775524f0b34f554545b1e3d73db2
Daniela Leguari <danielaleguari@outlook.com>
2023-02-09 10:27:57 -0300

    responsividade banners

--- src/components/BannerSobreASerie/index.tsx
+++ src/components/BannerSobreASerie/index.tsx
@@ -2,19 +2,21 @@ import styles from './BannerSobreASerie.module.css';
 import imagem2 from '../../assets/imagem2.png'
 
 const BannerSobreSerie = () => {
-
-
     return (
         <>
-            <main className={styles.banner}  >
-                <div className={styles.informacoes}>
-                    <img className={styles.bannerImg} src={imagem2} alt="Os Winchesters"></img>
-                    <h1 className={styles.titulo}>Os Winchesters</h1>
-                    <p className={styles.texto}>A Série "The Winchesters" é um spin-off de Supernatural. Sua estreia foi em Outubro deste ano.
-                         </p>
+            <div className={styles.banner}>
+                <div className={styles.apresentacao}>
+                    <h1 className={styles.titulo}>
+                        Os Winchesters
+                    </h1>
+                    <p className={styles.paragrafo}>
+                        A Série "The Winchesters" é um spin-off de Supernatural. Sua estreia foi em Outubro de 2022.
+                    </p>
                 </div>
-            </main>
-
+                <div className={styles.imagens}>
+                    <img className={styles.imagem} src={imagem2} alt="Os Winchesters" />
+                </div>
+            </div>
         </>
     )
 }
